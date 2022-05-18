13b331a5c6ff6e10b4accd86760f594a144b42a7
MicheleSavii <michelesavi987@gmail.com>
2022-05-18 08:59:56 +0100

    Michele Savi; 9:59; Lab 2.2; Creazione della classe Articolo

---
+++ AS2122_4E_INF_SaviMichele_GestioneArticoli/Articolo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2122_4E_INF_SaviMichele_GestioneArticoli
+{
+    internal class Articolo
+    {
+    }
+}
